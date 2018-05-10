2a5e13291d79003c8037fcab4f4cfcaa421827b2
Stefan <roofus95@yandex.ru>
2018-05-10 04:25:37 +0400

    fix 'delete_nothing_before' test stub

--- test/test_voice2text_comands.py
+++ test/test_voice2text_comands.py
@@ -4,7 +4,7 @@ import unittest
 MEDIA_DELETE = '/media_files/media_delete.wav'
 
 # 'command delete'
-MEDIA_DELETE = '/media_files/media_delete_nothing_before.wav'
+MEDIA_DELETE_NOTHING_BEFORE = '/media_files/media_delete_nothing_before.wav'
 
 
 class MyTestCase1(unittest.TestCase):
@@ -17,7 +17,7 @@ class MyTestCase1(unittest.TestCase):
     def test_command_delete_nothing_before(self):
         print('test_command_delete_nothing_before')
         expected_text = ''
-        actual_text = v2t(MEDIA_DELETE)
+        actual_text = v2t(MEDIA_DELETE_NOTHING_BEFORE)
         self.assertEqual(actual_text, expected_text)
 
 
