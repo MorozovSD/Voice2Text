f80921d96eebd99faae68d94f1b770a7125c7638
Stefan <roofus95@yandex.ru>
2018-05-10 04:19:30 +0400

    add '1word', 'sentence' and 'delete' test stub

---
+++ test/test_voice2text_comands.py
@@ -0,0 +1,25 @@
+import unittest
+
+# 'before command delete after'
+MEDIA_DELETE = '/media_files/media_delete.wav'
+
+# 'command delete'
+MEDIA_DELETE = '/media_files/media_delete_nothing_before.wav'
+
+
+class MyTestCase1(unittest.TestCase):
+    def test_command_delete(self):
+        print('test_command_delete')
+        expected_text = 'before after'
+        actual_text = v2t(MEDIA_DELETE)
+        self.assertEqual(actual_text, expected_text)
+
+    def test_command_delete_nothing_before(self):
+        print('test_command_delete_nothing_before')
+        expected_text = ''
+        actual_text = v2t(MEDIA_DELETE)
+        self.assertEqual(actual_text, expected_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
